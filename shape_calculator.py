824a31524cb6d3388bcd5e0dd46ce30b144172ef
daniel.boros <daniel.boros@ocado.com>
2021-05-01 13:15:27 +0100

    Implement Rectangle methods

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,34 +1,56 @@
 class Rectangle:
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def set_width(self):
-        pass
+        self._width = width
+        self._height = height
     
-    def set_height(self):
-        pass
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def set_width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def set_height(self, value):
+        self._height = value
     
     def get_area(self):
-        pass
-        # : Returns area (width * height)
+        return self.width * self.height
     
     def get_perimeter(self):
-        pass
-        # : Returns perimeter (2 * width + 2 * height)
+        return (self.width + self.height) * 2
     
     def get_diagonal(self):
-        pass
-        # : Returns diagonal ((width ** 2 + height ** 2) ** .5)
+        return (self.width ** 2 + self.height ** 2) ** 0.5
     
     def get_picture(self):
-        pass
-        # : Returns a string that represents the shape using lines of "*". The number of lines should be equal to the height and the number of "*" in each line should be equal to the width. There should be a new line (\n) at the end of each line. If the width or height is larger than 50, this should return the string: "Too big for picture.".
-    
-    def get_amount_inside(self):
-        pass
-        # : Takes another shape (square or rectangle) as an argument. Returns the number of times the passed in shape could fit inside the shape (with no rotations). For instance, a rectangle with a width of 4 and a height of 8 could fit in two squares with sides of 4.
+        # Returns a string that represents the shape using lines of "*".
+        # The number of lines should be equal to the height and the number of "*" in each line should be equal to the width.
+        # There should be a new line (\n) at the end of each line.
+        # If the width or height is larger than 50, this should return the string: "Too big for picture.".
+        if self.height > 50 or self.width > 50:
+            return "Too big for picture."
+        
+        oneLine = "*" * self.width
+        allLines = [oneLine for _ in range(0, self.height)]
+
+        return "\n".join(allLines)
+
+    def get_amount_inside(self, other):
+        # Takes another shape (square or rectangle) as an argument.
+        # Returns the number of times the passed in shape could fit inside the shape (with no rotations).
+        # For instance, a rectangle with a width of 4 and a height of 8 could fit in two squares with sides of 4.
+        
+        noFitWidth = self.width // other.width
+        noFitHeight = self.height // other.height
+
+        return noFitHeight * noFitWidth
 
     def __repr__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
